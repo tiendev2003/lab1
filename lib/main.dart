a09790bdd2bfc10edf13c350a545753b501c2315
tiendev2003 <tientc.21it@vku.udn.vn>
2024-08-18 21:52:27 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 47, 51, 115),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 30, 32, 54),
+        title: const Text('I Am Rich', style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Image.asset('assets/diamond.png'),
+      ),
+    );
+  }
+}
